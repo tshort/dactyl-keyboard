f7d80b26efbe7ca054377659f9d0510e16e1e19f
Adam Perlman <aperlman@quintevents.com>
2019-09-22 16:57:19 -0400

    Do not leave middle part of cutout

--- src/dactyl_keyboard/dactyl.clj
+++ src/dactyl_keyboard/dactyl.clj
@@ -81,15 +81,15 @@
                        (translate [(+ (/ 1.5 2) (/ keyswitch-width 2))
                                    0
                                    (/ plate-thickness 2)]))
-        kailh-cutout (->> (cube (/ keyswitch-width 3) 1.5 plate-thickness)
+        kailh-cutout (->> (cube (/ keyswitch-width 3) 1.6 plate-thickness)
                           (translate [0
-                                  (+ (/ 1.5 2) (+ (/ keyswitch-height 2) 0.2))
+                                  (+ (/ 1.5 2) (+ (/ keyswitch-height 2)))
                                   (/ plate-thickness)]))
         side-nub (->> (binding [*fn* 30] (cylinder 1 2.75))
                       (rotate (/ π 2) [1 0 0])
                       (translate [(+ (/ keyswitch-width 2)) 0 1])
-                      (hull (->> (cube 1.6 2.75 plate-thickness)
-                                 (translate [(+ (/ 1.6 2) (/ keyswitch-width 2))
+(hull (->> (cube 1.5 2.75 plate-thickness)
+                                 (translate [(+ (/ 1.5 2) (/ keyswitch-width 2))
                                              0
                                              (/ plate-thickness 2)]))))
         plate-half (union (difference top-wall kailh-cutout) left-wall)]
