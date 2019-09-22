e0034d41f2c8ba07ceea56b69384a2c25a5aa640
Adam Perlman <aperlman@quintevents.com>
2019-09-22 15:51:52 -0400

    Nicely fitting switches

--- create-models.sh
+++ create-models.sh
@@ -5,6 +5,7 @@ cp things/right-plate.scad things/right-4x5-plate.scad
 openscad -o things/right-4x5-plate.dxf things/right-4x5-plate.scad >/dev/null 2>&1 &
 openscad -o things/right-4x5.stl things/right-4x5.scad >/dev/null 2>&1 &
 openscad -o things/left-4x5.stl  things/left-4x5.scad >/dev/null 2>&1 &
+openscad -o things/switch.stl  things/switch.scad >/dev/null 2>&1 &
 
 patch -p1 < 4x6.patch 
 lein run src/dactyl_keyboard/dactyl.clj
